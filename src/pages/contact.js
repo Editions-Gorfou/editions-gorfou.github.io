5fec3227907203032d0dd857823984840c94ba96
Geoffrey ROYER <geoffrey.royer@gmail.com>
2025-07-26 17:48:21 +0300

    Update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,9 @@ export default function Contact() {
       <h1 className="font-medium leading-tight text-5xl mt-5 mb-5 text-center font-bold">Nous contacter</h1>
       <div className="container m-5 mx-auto">
         <p>Vous pouvez nous écrire directement à <a href="mailto:publication@editions-gorfou.fr">publication@editions-gorfou.fr</a>. Nous n’acceptons pas de manuscrit par la voie postale.</p>
+        <p><strong>Note importante :</strong>
+        cette adresse email est destinée à recevoir des manuscrits potentiels, or nous avons reçu de nombreuses sollicitations indésirables.
+        Toute tentative impersonnelle de démarchage commerciale résultera en classification permanente de pourriel. Merci de votre compréhension.</p>
       </div>
     </>
   )
--- src/pages/submit-draft.js
+++ src/pages/submit-draft.js
@@ -9,7 +9,7 @@ export default function SubmitDraft() {
         <p>Avant tout envoi, nous vous invitons à vérifier si votre manuscrit est en accord avec la <Link href="/editioral-line">ligne éditoriale</Link> des Éditions Gorfou.</p>
         <p>Vous devrez envoyer votre manuscrit directement à l’adresse suivante : <a href="mailto:publication@editions-gorfou.fr">publication@editions-gorfou.fr</a>.</p>
         <p>Dans votre email, <strong>merci d’ajouter toute information complémentaire utile à sa lecture</strong>. Une présentation succincte de l’auteur ainsi qu’un paragraphe accrocheur ne sont pas de refus.</p>
-        <p>Si vous n’obtenez pas de réponse au bout de deux mois, considérez que votre manuscrit n’a pas retenu votre attention. Toutefois nous nous efforcerons de vous fournir un retour aussi informatif que possible.</p>
+        <p>Si vous n’obtenez pas de réponse au bout de deux mois, considérez que votre manuscrit n’a pas retenu notre attention. Toutefois nous nous efforcerons de vous fournir un retour aussi informatif que possible.</p>
       </div>
     </>
   )
