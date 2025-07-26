a9b86d8d74d2f50706d09e534e560fd8bb55aade
Geoffrey ROYER <geoffrey.royer@gmail.com>
2025-07-26 17:55:40 +0300

    Update

--- src/pages/author/[slug].js
+++ src/pages/author/[slug].js
@@ -28,8 +28,8 @@ export async function getStaticProps({ params: { slug } }) {
 
 export default function AuthorPage({ frontmatter, content }) {
   return (
-    <div className="mx-auto m-5">
-      <h1 className="mt-5">{frontmatter.name}</h1>
+    <div className="mx-auto m-5 ml-5 mr-5">
+      <h1 className="mt-5 text-xl font-bold mb-8 text-center">{frontmatter.name}</h1>
       <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
     </div>
   );
